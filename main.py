45f2a32ee26b94b75e36ae8a12006cd4313896b9
PhilZPhaZ <bour.phileas@gmail.com>
2023-02-04 12:31:40 +0100

    Started implemanting the addition

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 class Number:
     def __init__(self, value):
-        self.value = value
-        
+        self._value = str(value)
+        self._number_of_decimal = self._value[::-1].find('.')
+        self._number_list = [self._value.split('.')[0], self._value.split('.')[1]]
+    
+    def __str__(self):
+        return self._value
+    
     def __add__(self, other):
         return self.value + other.value
+
+        
+n1 = Number(0.1)
+n2 = Number(0.2)
+
+print(n2)
