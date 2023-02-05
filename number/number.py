f5254efdf76b345a20910b80e7feabe9d5533903
PhilZPhaZ <bour.phileas@gmail.com>
2023-02-05 15:20:08 +0100

    Continue working on addition

--- number/number.py
+++ number/number.py
@@ -3,7 +3,8 @@ class Number:
         self._value = str(float(value))
         self._number_of_decimal = self._value[::-1].find('.')
         self._number_list = [int(self._value.split('.')[0]) if int(self._value.split('.')[0]) <= 0 else int(self._value.split('.')[0]), int(self._value.split('.')[1])]
-        self._decimal_signe = self._number_list[1] if self._number_list[0] > 0 else -self._number_list[1]
+        self._decimal_signe = None
+        self._signe = '-' if value < 0 else None
         self._number_final = []
         
     def __str__(self):
@@ -14,8 +15,11 @@ class Number:
         self._get_max_decimal(other)
         self._add_zeros(other)
         
+        self._decimal_signe = self._number_list[1] if float(self._value) > 0 else -self._number_list[1]
+        other._decimal_signe = other._number_list[1] if float(other._value) > 0 else -other._number_list[1]
+        
         # positif - positif
-        if self._number_list[0] >= 0 and other._number_list[0] >= 0:
+        if self._number_list[0] > 0 and other._number_list[0] > 0:
             self._number = self._number_list[0] + other._number_list[0]
             self._decimal = self._number_list[1] + other._number_list[1]
 
@@ -31,13 +35,20 @@ class Number:
                 self._decimal = int(str(self._decimal)[1:])
                 self._number += 1
             self._number = -self._number
+        # 0 - 0
+        elif self._number_list[0] == 0 or other._number_list[1] == 0:
+            self._number = 0
+            self._decimal = 0
         # positif - negatif | negatif - positif
         else:
             self._find_signe(other)
             self._number = abs(self._number_list[0]) - abs(other._number_list[0])
             self._decimal = abs(self._decimal_signe) - abs(other._decimal_signe)
+            print(self._decimal)
+            if self._decimal < 0 and self._number > 0:
+                self._number -= 1
+                self._decimal = 10**self._max_decimal_n - abs(self._decimal)
             
-        
         return self._number, self._decimal
 
     # ---------- Subtraction ----------
@@ -47,13 +58,12 @@ class Number:
     
     # ---------- Other Methods ----------
     def _find_signe(self, other):
-        self._number1 = float(f'{self._number_list[0]}.{self._number_list[1]}')
-        self._number2 = float(f'{other._number_list[0]}.{other._number_list[1]}')
+        self._number1 = float(self._value)
+        self._number2 = float(other._value)
         if abs(self._number1) > abs(self._number2):
-            if self._number1 < 0:
-                self._signe = '-'
-        elif self._number2 < 0:
-            self._signe = '-'
+            self._signe = self._signe
+        elif abs(self._number2) > abs(self._number1):
+            self._signe = other._signe
     
     def _get_max_decimal(self, other):
         self._max_decimal_n = self._number_of_decimal if self._number_of_decimal > other._number_of_decimal else other._number_of_decimal
@@ -67,6 +77,6 @@ class Number:
                 other._number_list[1] = int(f'{str(other._number_list[1])}0')
     
 
-n1 = Number(10)
-n2 = Number(-18.3)
+n1 = Number(-5.5)
+n2 = Number(7.5)
 print(n1 + n2)
